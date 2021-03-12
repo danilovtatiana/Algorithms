ea0d85644c0fcd81df9e55b195e66407101a12e9
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-12 11:42:47 +0200

    final version

--- weid_not_weird.py
+++ weid_not_weird.py
@@ -12,7 +12,7 @@ n = int(input())
 if n % 2 == 0:
     if 2 <= n < 5 or n > 20:
         print("Not Weird")
-    elif 6 <= n < 20:
+    elif 6 <= n <= 20:
         print("Weird")
 else:
     print("Weird")
