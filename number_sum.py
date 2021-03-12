26c68ab92af2eecfa34877a34433f0ca7a99a609
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-12 18:32:02 +0200

    calculate the sum of the random number

--- number_sum.py
+++ number_sum.py
@@ -8,3 +8,20 @@ while n != 0:
     n = int(n / 10)
     sum += last_digit
 print(sum)
+
+
+#calculate the sum of all digits from the random number
+
+from random import randint
+
+n = randint(100,999)
+print("n = ",n)
+
+sum = 0
+
+while n != 0:
+    last_digit = n % 10
+    n = int(n / 10)
+    sum += last_digit
+print(sum)
+
